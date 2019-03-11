d2bb4c431754b28d1bb80e5b4e4a7e3c2f767887
Tim Dommett <timdom11@gmail.com>
2019-03-11 18:43:00 +0200

    created routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,18 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  # namespace :api, defaults: { format: :json } do
+    resources :users do
+      resources :posts, only: [:index]
+    end
+
+    resource :session, only: [:new, :create, :destroy]
+
+    post '/search', to: 'users#search'
+
+    resources :posts
+    resources :votes, only: [:create, :destroy]
+    delete '/votes', to: 'votes#destroy'
+    resources :comments
+  # end
+  # root to: 'root#root'
 end
