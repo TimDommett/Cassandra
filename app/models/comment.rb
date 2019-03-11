190b3446d30506be8f4a8113f9209cd5100b33e0
Tim Dommett <timdom11@gmail.com>
2019-03-11 18:28:09 +0200

    created models and validations

---
+++ app/models/comment.rb
@@ -0,0 +1,29 @@
+# == Schema Information
+#
+# Table name: likes
+#
+#  id         :integer          not null, primary key
+#  user_id    :integer          not null
+#  chirp_id   :integer          not null
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+
+class Comment < ApplicationRecord
+  # This is actually redundant since Rails 5+ will automatically validate
+  # belongs to relationships, and the foreign keys will be implicitly
+  # checked as a result
+  validates :user_id, :post_id, presence: true
+
+  belongs_to :post,
+    primary_key: :id,
+    foreign_key: :post_id,
+    class_name: :Post
+    # optional: true
+
+  belongs_to :user,
+    primary_key: :id,
+    foreign_key: :user_id,
+    class_name: :User
+    # optional: true
+end
---
+++ app/models/post.rb
@@ -0,0 +1,46 @@
+# == Schema Information
+#
+# Table name: chirps
+#
+#  id         :integer          not null, primary key
+#  body       :text             not null
+#  author_id  :integer          not null
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+
+class Post < ApplicationRecord
+  validates :title, presence: true
+  validates :description, presence: true
+  # validate :chirp_too_long
+
+  has_many :votes,
+    primary_key: :id,
+    foreign_key: :post_id,
+    class_name: :Vote
+
+  has_many :comments,
+    primary_key: :id,
+    foreign_key: :post_id,
+    class_name: :Comment
+
+# check the next two
+  has_many :voters,
+    through: :votes,
+    source: :user
+
+  has_many :commentors,
+    through: :votes,
+    source: :user
+
+  belongs_to :user,
+    primary_key: :id,
+    foreign_key: :user_id,
+    class_name: :User
+
+  # def chirp_too_long
+  #   if body.length > 140
+  #     errors[:body] << "too long"
+  #   end
+  # end
+end
