043ae1076b873ac8f92e53a38bd287299bc04afe
Tim Dommett <timdom11@gmail.com>
2019-03-11 17:26:48 +0200

    created posts table migration

--- db/migrate/20190311151850_create_posts_table.rb
+++ db/migrate/20190311151850_create_posts_table.rb
@@ -1,6 +1,12 @@
 class CreatePostsTable < ActiveRecord::Migration[5.2]
   def change
-    create_table :posts_tables do |t|
+    create_table :posts do |t|
+      t.string :title, null: false
+      t.text :description, null: false
+      t.integer :user_id, null: false
+
+      t.timestamps
     end
+    add_index :posts, :user_id
   end
 end
