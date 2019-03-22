d2851f39e5180c0ce09a531105166d2af5da83ab
Tim Dommett <timdom11@gmail.com>
2019-03-23 00:52:53 +0200

    updated css styling

--- app/assets/javascripts/bundle.js
+++ app/assets/javascripts/bundle.js
@@ -370,9 +370,11 @@ __webpack_require__.r(__webpack_exports__);
     className: "about"
   }, react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("div", {
     className: "navbar-spacer"
-  }), react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("h1", null, "Created as a testament to the power of free education."), react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("p", {
+  }), react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("h1", {
+    className: "about-title"
+  }, "Created as a testament to the power of free education."), react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("p", {
     className: "about-paragraph"
-  }, " Cassandra was created entirely using completely free online recorces by a developer with no previous experience to illustrate the power of free online education. Cassandra was desgined to be a hub for free online learning with the hope that it will make sorting through and finding quality educational recources easier for those looking. We live in a time where the cost of quality education is so high that not many people can afford it, leading to institutional class devides and unequal opportunity to jobs and hence a system whioch makes it extremely hard for individuals to break out of the income brackets that they were born into."));
+  }, " Cassandra was created entirely using completely free online recorces by a developer with no previous experience to illustrate the power of free online education. Cassandra was desgined to be a hub for free online learning with the hope that it will make sorting through and finding quality educational recources easier for those looking. We live in a time where the cost of quality education is so high that not many people can afford it, leading to institutional class devides and unequal opportunity to jobs and hence a system whioch makes it extremely hard for individuals to break out of the income brackets that they were born into. You should add a draewingon the left here to take up the left third"));
 });
 
 /***/ }),
