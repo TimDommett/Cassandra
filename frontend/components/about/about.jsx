020808264658bdc3777d966396edfdbafbf4d497
Tim Dommett <timdom11@gmail.com>
2019-04-16 03:23:34 +0200

    cleaning code

--- frontend/components/about/about.jsx
+++ frontend/components/about/about.jsx
@@ -7,7 +7,6 @@ export default () => (
     <h1 className="about-title">Created as a testament to the power of free education.</h1>
     <p className="about-paragraph"> Cassandra was created entirely using completely free online recorces by a developer with no previous experience to illustrate the power of free online education. Cassandra was desgined to be a hub for free online learning with the hope that it will make sorting through and finding quality educational recources easier for those looking.
     We live in a time where the cost of quality education is so high that not many people can afford it, leading to institutional class devides and unequal opportunity to jobs and hence a system whioch makes it extremely hard for individuals to break out of the income brackets that they were born into.
-    You should add a draewingon the left here to take up the left third
     </p>
   </div>
 )
